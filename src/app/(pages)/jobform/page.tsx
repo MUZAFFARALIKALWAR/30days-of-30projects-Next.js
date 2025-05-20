65ec58cd768674daa8c8e33386d32b5128a59bb4
Muzaffar Ali <muzafark834@gmail.com>
2025-05-20 14:46:30 +0500

    add job form

---
+++ src/app/(pages)/jobform/page.tsx
@@ -0,0 +1,12 @@
+import Jobform from '@/components/jobform'
+import React from 'react'
+
+function JobForm() {
+  return (
+    <div>
+        <Jobform/>
+    </div>
+  )
+}
+
+export default JobForm
---
+++ src/components/jobform.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Jobform() {
+  return (
+    <div>Jobform</div>
+  )
+}
+
+export default Jobform
